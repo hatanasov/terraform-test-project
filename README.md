fe3c1c3624563448f329eb498713412291e48c69
Hristo Atanasov <hristo.atanasov@softwareone.com>
2025-07-02 11:21:54 +0300

    Updated README

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # This repo is for testing  purpose
 #
-#
--- terraform/cd/data.tf
+++ terraform/cd/data.tf
@@ -3,3 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_codestarconnections_connection" "gh" {
   name = "cicd-image-factory-repo"
 }
+
+output "connection" {
+  value = data.aws_codestarconnections_connection.gh.arn
+}
